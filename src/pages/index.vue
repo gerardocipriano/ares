21fdf8f4c79a3f070fc0438db618073762aaab1c
Marawan <marawan.emadragab@studio.unibo.it>
2022-12-07 10:58:55 +0100

    changed css

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,15 +24,10 @@ export default defineComponent({
 
 <template>
   <!-- <h1>ARES</h1> -->
-  <img src="../static/img/logo.png" />
-  <div class="m-5">
-    <!-- <NuxtPage /> -->
-    <div class="btn btn-success">Success</div>
-    <div class="btn btn-danger">Danger</div>
-    <div class="btn btn-warning">Warning</div>
-  </div>
-
-  <section>
+    <center>
+      <img src="../static/img/logo.png"/>
+    </center>
+    <section>
     <table>
       <tr>
         <th id="id">id</th>
